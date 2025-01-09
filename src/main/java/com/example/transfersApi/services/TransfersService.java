949ab45ce6265c659f29d527771be3cb64f15d6c
Gabriel Molin <molinvinicius@gmail.com>
2025-01-09 13:14:40 -0300

    feat: transfer build data

--- src/main/java/com/example/transfersApi/services/TransfersService.java
+++ src/main/java/com/example/transfersApi/services/TransfersService.java
@@ -11,27 +11,65 @@ import org.springframework.stereotype.Service;
 
 import com.example.transfersApi.dto.TransfersDTO;
 import com.example.transfersApi.models.TransferModel;
+import com.example.transfersApi.models.UserModel;
 import com.example.transfersApi.repositories.TransfersRepository;
+import com.example.transfersApi.repositories.UsersRepository;
+import com.example.transfersApi.utils.calcs.Calcs;
 
 @Service
 public class TransfersService {
 
-  private final TransfersRepository TransfersRepository; 
+  private final TransfersRepository transfersRepository; 
   private final ModelMapper modelMapper;
+  private final UsersRepository usersRepository;
 
-  public TransfersService(TransfersRepository TransfersRepository, ModelMapper modelMapper) {
-    this.TransfersRepository = TransfersRepository;
+  public TransfersService(TransfersRepository transfersRepository, ModelMapper modelMapper, UsersRepository usersRepository) {
+    this.transfersRepository = transfersRepository;
     this.modelMapper = modelMapper;
+    this.usersRepository = usersRepository;
   }
 
-  public TransferModel create(TransfersDTO Transfer){
-    TransferModel TransferModel = modelMapper.map(Transfer, TransferModel.class);
-    TransferModel result = TransfersRepository.save(TransferModel);
-    return result;
+  public TransferModel create(TransfersDTO transfer){
+    TransferModel transferModel = modelMapper.map(transfer, TransferModel.class);
+    Optional<UserModel> accountOrigin, accountDestination;
+    
+    try {
+      accountOrigin = usersRepository.findByAccountNumberAndIsDeletedFalse(transfer.getOriginAccountNumber());
+      accountDestination = usersRepository.findByAccountNumberAndIsDeletedFalse(transfer.getDestinationAccountNumber());
+
+    } catch (Exception e) {
+      throw new RuntimeException("Erro ao buscar usuarios");
+    }
+
+    if(!accountOrigin.isEmpty() && !accountDestination.isEmpty()){
+      UserModel existingOrigin = accountOrigin.get();
+      UserModel existingDestination = accountDestination.get();
+
+      transferModel.setAccountOrigin(existingOrigin);
+      transferModel.setAccountDestination(existingDestination);
+
+    } else {
+      throw new RuntimeException("Erro ao montar dados");
+    }
+
+    Calcs calcsClass = new Calcs();
+    Double calculatedFee = calcsClass.calculateFee(transfer);
+    Double finalValue = (transfer.getOriginalValue() - calculatedFee);
+
+    transferModel.setFinalValue(finalValue);
+    transferModel.setFee(calculatedFee);
+
+    try {
+      TransferModel result = transfersRepository.save(transferModel);
+      return result;
+    } catch (Exception e) {
+      throw new RuntimeException("Erro ao salvar dados");
+    }
+
   }
 
   public TransferModel update(TransfersDTO TransferDTO, UUID id) {
-    Optional<TransferModel> optionalTransfer = TransfersRepository.findById(id);
+    Optional<TransferModel> optionalTransfer = transfersRepository.findById(id);
 
     if (optionalTransfer.isEmpty()) {
         throw new RuntimeException("Usuário não encontrado com o ID: " + id);
@@ -40,11 +78,11 @@ public class TransfersService {
     TransferModel existingTransfer = optionalTransfer.get();
     modelMapper.map(TransferDTO, existingTransfer);
 
-    return TransfersRepository.save(existingTransfer);
+    return transfersRepository.save(existingTransfer);
   }
 
   public TransferModel delete(UUID id) {
-    Optional<TransferModel> foundTransfer = TransfersRepository.findById(id);
+    Optional<TransferModel> foundTransfer = transfersRepository.findById(id);
 
     if (foundTransfer.isEmpty()) {
         throw new RuntimeException("Usuário não encontrado com o ID: " + id);
@@ -53,19 +91,19 @@ public class TransfersService {
     TransferModel existingTransfer = foundTransfer.get();
     existingTransfer.setDeleted(true);
 
-    return TransfersRepository.save(existingTransfer);
+    return transfersRepository.save(existingTransfer);
   }
   
   public Page<TransferModel> findAll(int page, int size) {
     Pageable pageable = PageRequest.of(page, size);
-    return TransfersRepository.findByIsDeletedFalse(pageable);
+    return transfersRepository.findByIsDeletedFalse(pageable);
   }
 
   public TransferModel findOne(UUID id){
-    Optional<TransferModel> optionalTransfer = TransfersRepository.findByIdAndIsDeletedFalse(id);
+    Optional<TransferModel> optionalTransfer = transfersRepository.findByIdAndIsDeletedFalse(id);
 
     if (optionalTransfer.isEmpty()) {
-        throw new RuntimeException("Usuário não encontrado com o ID: " + id);
+      throw new RuntimeException("Usuário não encontrado com o ID: " + id);
     }
 
     TransferModel existingTransfer = optionalTransfer.get();
